7d58bd52f5b8bac6efec06ed4e4d9e05820e1e6d
4-o-4 <yuzhakov.lev@gmail.com>
2024-07-15 16:49:36 +0400

    refactor: delete component

--- task-starter/src/main/java/com/example/config/LoggingProperties.java
+++ task-starter/src/main/java/com/example/config/LoggingProperties.java
@@ -1,9 +1,7 @@
 package com.example.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties("log")
 public class LoggingProperties {
     /**
