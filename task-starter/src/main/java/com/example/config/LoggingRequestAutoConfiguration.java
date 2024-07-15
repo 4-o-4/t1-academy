75c9543d3b587aa7a1a2ea6a75a87d0a26d4279f
4-o-4 <yuzhakov.lev@gmail.com>
2024-07-15 16:41:18 +0400

    feat: add check properties

--- task-starter/src/main/java/com/example/config/LoggingRequestAutoConfiguration.java
+++ task-starter/src/main/java/com/example/config/LoggingRequestAutoConfiguration.java
@@ -1,12 +1,14 @@
 package com.example.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ComponentScan("com.example")
+@ConditionalOnProperty(value="log.level")
 public class LoggingRequestAutoConfiguration implements WebMvcConfigurer {
     private final HandlerInterceptor handlerInterceptor;
 
